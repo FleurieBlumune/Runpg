bba2c71aa068dff3c7a5e99997391436537ba7ec
FleurieBlumune <brittany.stone89@gmail.com>
2014-08-12 19:21:51 -0500

    Add existing project to Git

---
+++ Assets/Managers/StatManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Run.Managers
+{
+    public class StatManager : MonoBehaviour
+    {
+        public static String CharacterLevelString = "CharLevel";
+        public static String PlayerXpString = "CharXp";
+
+        public int CharHp, CurCharHp, CharAtk, CharDef, CharXpToLevel, CharXp, CurEnemyLevel;
+        public int CurPlayerLevel = PlayerPrefs.GetInt(CharacterLevelString);
+
+        void Start()
+        {
+
+            //if (PlayerPrefs.GetInt(PlayerLevelString) == 0)
+                SetPlayerLevelToOne();
+
+            if (PlayerPrefs.GetInt(PlayerXpString) == 0)
+                SetPlayerInitialXp();
+
+            SetPlayerStatsBasedOnLevel();
+        }
+
+        void Update()
+        {
+            if (CharXp > CharXpToLevel)
+                PlayerLevelUp();
+
+            if (CurCharHp <= 0)
+                GameEventManager.TriggerGameOver();
+        }
+
+        #region Player Level Functions
+
+        void PlayerLevelUp()
+        {
+            PlayerPrefs.SetInt(CharacterLevelString, CurPlayerLevel + 1);
+            SetPlayerStatsBasedOnLevel();
+        }
+
+        void SetPlayerLevelToOne()
+        {
+            PlayerPrefs.SetInt(CharacterLevelString, 1);
+        }
+
+        void SetPlayerStatsBasedOnLevel()
+        {
+            //Check out that hackish shit lol
+            CharHp = CurCharHp = CharAtk = CharDef = (CurPlayerLevel * 2);
+            CharXpToLevel = CurPlayerLevel * 100;
+        }
+
+        void SetPlayerInitialXp()
+        {
+            PlayerPrefs.SetInt(PlayerXpString, 10);
+        }
+
+        #endregion
+
+        #region enemy level functions
+
+        void SetEnemyLevel()
+        {
+            CurEnemyLevel = (CurPlayerLevel * 2) - 1;
+        }
+
+        void SetEnemyTraits()
+        { }
+        #endregion
+    }
+}
